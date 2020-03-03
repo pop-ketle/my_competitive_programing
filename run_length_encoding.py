b72de0bfa4df2560460e124bb883072acf7e13f9
pop-ketle <36680011+pop-ketle@users.noreply.github.com>
2020-03-03 23:34:17 +0900

    Update run_length_encoding.py

--- run_length_encoding.py
+++ run_length_encoding.py
@@ -8,3 +8,14 @@ def rle(S):
     else:
       cnt+=1
   return rle+now+str(cnt)
+
+def rle(S):
+  # ランレングス圧縮 AAABCDEE->A3BCDE2
+  cnt,now,rle=1,S[0],''
+  for i in range(1,len(S)):
+    if now!=S[i]:
+      rle+=now if cnt==1 else now+str(cnt)
+      cnt,now=1,S[i]
+    else:
+      cnt+=1
+  return rle+now if cnt==1 else rle+now+str(cnt)
